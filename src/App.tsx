b0b6adda642b2a392f7023c3d09b20ff617f2e9d
Maxime Pierre <90000020+maximepierregit@users.noreply.github.com>
2024-06-17 21:26:42 -0400

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
+import SpotifyPlayer from 'react-spotify-player';
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const size = {
+    width: 500,
+    height: 200,
+  };
+  const view = 'list'; // or 'coverart'
+  const theme = 'black'; // or 'white'
+   
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="App">
+      <div className="section" id="section1">
+        <h1 id='main-title'>SPOTEAM</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+          <SpotifyPlayer
+            uri="spotify:track:5SuOikwiRyPMVoIQDJUgSV"
+            size={size}
+            view={view}
+            theme={'black'}
+              />
+          <iframe id='embedded-player' src="https://open.spotify.com/embed/track/5dkSQ8ExtSaS5diqiQosOk?utm_source=generator&theme=0" width="60%" height="152" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="section" id="section2">
+        <h1>Section 2</h1>
+        <p>This is the second section.</p>
+      </div>
+      <div className="section" id="section3">
+        <h1>Section 3</h1>
+        <p>This is the third section.</p>
+      </div>
+    </div>
   );
 }
 
